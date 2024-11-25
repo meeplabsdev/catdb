4c6ee1d6358490993347b6706b3640e617acb13b
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2024-11-25 21:12:57 +0000

    script

--- meta.py
+++ meta.py
@@ -10,13 +10,14 @@ for i in meta["images"]:
     seenUrls.append(i["file"])
 
 images = os.listdir("images")
-for i in images:
+for num, i in enumerate(images):
     url = f"https://github.com/meeplabsdev/catdb/blob/main/images/{i}?raw=true"
 
     if url not in seenUrls:
         subprocess.run(["/usr/bin/kitty", "icat", os.path.abspath(f"images/{i}")])
+        print(f"{num+1} of {len(images)}")
         tags = input("Tags (comma-separated): ").strip().split(",")
-        if len(tags) > 0 and tags[0] != "":
+        if len(tags) > 0:
             meta["images"].append({"file": url, "tags": tags})
 
             for tag in tags:
